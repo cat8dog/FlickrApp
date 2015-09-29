1986d58fbfcd52f75282a4c0b452e6ccc35edb8e
cat8dog <catherine.reyto@gmail.com>
2015-09-29 11:53:51 -0400

    // trying to switch to new branch

--- FlickrApp/FlickrApp/RootViewController.swift
+++ FlickrApp/FlickrApp/RootViewController.swift
@@ -13,7 +13,7 @@ class RootViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // test git
+
 
         // Do any additional setup after loading the view.
     }
